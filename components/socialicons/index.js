bc97bd7f0838138b2a65ed8d4c859ed8a988accb
Alvin Nutor <alvinnutor@Rachaels-Mac-book-Pro.local>
2023-03-03 17:09:56 +0000

    Initial commit

---
+++ components/socialicons/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./style.css";
+import {
+  FaGithub,
+  FaFigma,
+  FaPhone,
+  FaLinkedin,
+  FaYoutube,
+  FaTwitch,
+} from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export const Socialicons = (params) => {
+  return (
+    <div className="stick_follow_icon">
+      <ul>
+        {socialprofils.twitter && (
+          <li>
+            <a href={socialprofils.twitter}>
+              <FaFigma />
+            </a>
+          </li>
+        )}
+        {socialprofils.github && (
+          <li>
+            <a href={socialprofils.github}>
+              <FaGithub />
+            </a>
+          </li>
+        )}
+        {socialprofils.facebook && (
+          <li>
+            <a href={socialprofils.facebook}>
+              <FaPhone />
+            </a>
+          </li>
+        )}
+        {socialprofils.linkedin && (
+          <li>
+            <a href={socialprofils.linkedin}>
+              <FaLinkedin />
+            </a>
+          </li>
+        )}
+        {socialprofils.youtube && (
+          <li>
+            <a href={socialprofils.youtube}>
+              <FaYoutube />
+            </a>
+          </li>
+        )}
+        {socialprofils.twitch && (
+          <li>
+            <a href={socialprofils.twitch}>
+              <FaTwitch />
+            </a>
+          </li>
+        )}
+      </ul>
+      <p>Get In Touch</p>
+    </div>
+  );
+};
---
+++ content_option.js
@@ -0,0 +1,165 @@
+const logotext = "ALVIN SENA NUTOR";
+const meta = {
+    title: "A.S.N Portfolio",
+    description: "Portfolio_00 :: Full Stack devloper portfolio",
+};
+
+const introdata = {
+    title: "I’m Alvin Sena Nutor",
+    animated: {
+        first: "I love Security",
+        second: "I code cool websites & Apps",
+        third: "I develop UI & UX Designs",
+    },
+    description: "I'm a Professional full-Stack developer responsible for working on both front-end and back-end development processes by designing, developing, and maintaining fully-fledged and functioning platforms with databases or servers",
+    your_img_url: "https://plus.unsplash.com/premium_photo-1674582743901-7e438e0e5ea0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=894&q=80",
+};
+const dataabout = {
+    title: "Personal Profile",
+    aboutme: "I am a professionally qualified fire engineer with 7 years experience. I have recently achieved RTITB accreditation in the use of Counterbalance fork lift trucks and I am seeking employment that will make best use of my skills and allow me to develop them further. I am determined and enthusiastic, I have developed good planning & organisational skills and am confident working independently or as part of a team. I am flexible regarding working hours and am able to work a range of shifts",
+};
+const worktimeline = [{
+        jobtitle: "Chief Information Security Officer",
+        where: "Dubai",
+        date: "2030 - Present",
+    },
+    {
+        jobtitle: "Cybersecurity Specialist",
+        where: "USA",
+        date: "2023 - 2030",
+    },
+    {
+        jobtitle: "Full-Stack Developer",
+        where: "Ghana",
+        date: "2018 - 2023",
+    },
+];
+
+const skills = [{
+        name: "React",
+        value: 98,
+    },
+    {
+        name: "(Web Development) = TypeScript , HTML , CSS , Javascript, Python , SQL ", 
+        value: 85,
+    },
+    {
+        name: "UX & UI Design",
+        value: 80,
+    },
+    {
+        name: "Image / Photo editing ",
+        value: 75,
+    },
+    {
+        name: "CyberSecurity",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: " Love to create user-friendly interfaces that enable users to understand how to use complex technical products.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "I'm involved in the conceptualization, design, and development of software for phones, smart watches, tablets and more.",
+    },
+    {
+        title: "Web Development",
+        description: "Awesome in creating high performing and responsive website's with contents for the site.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://images.unsplash.com/photo-1629752187687-3d3c7ea3a21b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80",
+        desctiption: "FIGMA.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1573867639040-6dd25fa5f597?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+        desctiption: "FIGMA Designs",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1518349619113-03114f06ac3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+        desctiption: "FIGMA.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1545065695-64b67d551e68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1546&q=80",
+        desctiption: "FIGMA.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1607799279861-4dd421887fb3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+        desctiption: "CODES.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80",
+        desctiption: "CODES.",
+        link: "#",
+    },
+
+    {
+        img: "https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+        desctiption: "CODES.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1551033406-611cf9a28f67?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+        desctiption: "CODES.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1607706009771-de8808640bcf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+        desctiption: "WEB DEVELOPMENT.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1550751827-4bd374c3f58b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+        desctiption: "SECURITY.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1562408590-e32931084e23?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+        desctiption: "SECURITY.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+        desctiption: "SECURITY.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
